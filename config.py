0dc35e2ce782499156703edf53d3484dfcabb3dd
Gil Shotan <gilsho@cs.stanford.edu>
2020-03-20 23:58:34 -0700

    updated heroku app name

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ class DevConfig(object):
   DEV = True
   CACHE_TYPE = 'simple'
   HOSTNAME = 'http://localhost'
-  ROOT_URL = 'http://localhost:5000/'
+  ROOT_URL = 'http://localhost:5000'
   MEMCACHIER_SERVERS = ['127.0.0.1']
 
 class ProductionConfig(object):
   DEV = False
-  HOSTNAME = 'https://herokuapp.com/'
-  ROOT_URL = 'https://herokuapp.com/'
+  HOSTNAME = 'https://israeli-whist.herokuapp.com/'
+  ROOT_URL = 'https://israeli-whist.herokuapp.com/'
   CACHE_TYPE = 'saslmemcached'
   MEMCACHIER_SERVERS = os.environ.get('MEMCACHIER_SERVERS', '').split(',')
   MEMCACHIER_USERNAME = os.environ.get('MEMCACHIER_USERNAME')
